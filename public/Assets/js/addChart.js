ffefa7ed8161140ef4cd873a5b9bb82411610639
Casini Lorenzo <casinilorenzo@protonmail.com>
2018-12-20 20:44:56 +0100

    added ajax call for dashboard and chart

---
+++ public/Assets/js/addChart.js
@@ -0,0 +1,95 @@
+/*each product is an array with this pattern:
+ name producer_id qnt prize image product_id companyName */
+$(function () {
+
+     var products = [];
+     var firstExecution = true;
+
+     if(firstExecution){
+        recoverChart();
+        firstExecution = false;
+     }
+
+    $(".view-link").click(function (ev) {
+        if ($(event.target).attr("class") === "view-link shutter") {
+            var parent = $(ev.target).parent();
+            var src = parent.find("img").attr("src");
+            var name = parent.find("h4").text();
+            var price = parent.find("p").text();
+            var producer = parent.find("#producer_id").attr("value");
+            var product_id = parent.find("#product_id").attr("value")
+            var companyName = parent.find("h7").text();
+            var match = false;
+
+            products.forEach(function (p) {
+                if (p[0] === name && p[1] === producer) {
+                    p[2] = p[2] + 1;
+                    match = true;
+                }
+            });
+
+            if (match === false) {
+                var array = [name, producer, 1, price, src, product_id, companyName];
+                products.push(array);
+            }
+        }
+        updateChart();
+    });
+
+
+    /*$("#checkoutButton").click(function () {
+
+    });*/
+
+    function updateChart() {
+        var children = $(".media-list").children();
+        children.each(function () {
+            $(this).remove();
+        });
+        products.forEach(function (p) {
+
+            var newElem = '<li class="media"><img class="pull-left" src="' + p[4] +
+                '" alt=""><div class="media-body"><h6 class="name">' + p[0] +
+                '</h6><h6><span>' + p[3] + '</span> <span  class = "chartPrize">' + p[2] + '</span><h6><a> <i class="fas fa-trash-alt"></i></a></div></li>'
+            $(".media-list").append(newElem);
+        });
+
+        $("a").on("click", ".fa-trash-alt", function (ev) {
+            var elem = $(ev.target).parentsUntil("ul");
+            var name = elem.find("h6.name").text();
+            for (i = 0; i < products.length; i++) {
+                if (products[i][0] === name) {
+                    products.splice(i, 1);
+                }
+            }
+            updateChart();
+        });
+
+        chartChanged();
+    }
+
+    function chartChanged(){
+        $.ajax({
+            type: 'POST',
+            data: {products: products},
+            url: '/Shop/updateCart',
+            dataType: 'json',
+            success: function(msg){
+            }
+        });
+    }
+
+    function recoverChart(){
+        console.log("First Execution");
+        $.ajax({
+            type: 'POST',
+            url: '/Shop/requestChart',
+            dataType: 'json',
+            success: function(array){
+                products = array;
+                updateChart();
+            }
+        });
+    }
+
+});
--- public/Assets/js/stickbar.js
+++ public/Assets/js/stickbar.js
@@ -2,12 +2,7 @@ $(function(){
     var yourNavigation = $(".navbar");
     stickyDiv = "sticky";
     yourHeader = $("header").height();
-    console.log(yourHeader);
-    console.log(yourNavigation);
-    console.log(window);
-    console.log(this);
     $(window).scroll(function () {
-        console.log($(this).scrollTop());
         if ($(this).scrollTop() > yourHeader) {
            // yourNavigation.removeClass("navbar-default");
             yourNavigation.addClass("sticky");
